434258a4c1a54a4b6500e63fc1d6163864f1a053
Chirayu Oli <chirayuoli56@gmail.com>
2020-09-12 11:56:56 +0545

    splitted firebase  utils

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,17 +1,8 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
-
-const config = {
-  apiKey: "AIzaSyCt31iFBiSRF8rrTkUzm5fzKlHaYCe1foo",
-  authDomain: "petsnepaldemo.firebaseapp.com",
-  databaseURL: "https://petsnepaldemo.firebaseio.com",
-  projectId: "petsnepaldemo",
-  storageBucket: "petsnepaldemo.appspot.com",
-  messagingSenderId: "983812644887",
-  appId: "1:983812644887:web:488c2ee00ca4c6ac863c52",
-  measurementId: "G-0NN488KHDZ",
-};
+import FirebaseConfig from "./firebase.config";
+const config = FirebaseConfig;
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
